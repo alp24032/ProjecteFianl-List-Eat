2670839e62918ca30347a43b73e45d907919137f
alp24032 <84396222+alp24032@users.noreply.github.com>
2023-05-31 23:51:33 +0200

    Delete ProjecteFinal directory

--- ProjecteFinal/LogIn_&_SignUp/addGroup.js
+++
@@ -1,7 +0,0 @@
-let formGroup = new bootstrap.Modal(document.getElementById('formGroup'));
-function showCreateGroup(){
-    formGroup.show();
-}
-function hideCreateGroup(){
-    formGroup.hide();
-}
